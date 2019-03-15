31f0d0aee181eb9996eb8626ccb0d15eb68b1509
rhoang57 <rhoang57@gmail.com>
2019-03-14 19:29:25 -0700

    WriteCard component added

--- client/src/components/WriteCard/index.js
+++ client/src/components/WriteCard/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom"
 
 function WriteCard(props) {
     return (
--- client/src/pages/BrowsePage.js
+++ client/src/pages/BrowsePage.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import NewBlockPreview from "../components/NewBlockPreview";
-import BlockPreview from "../components/BlockPreview";
+import WriteCard from "../components/WriteCard";
 import { Container, Row, Col } from "../components/Grid";
 
 class BrowsePage extends Component {
+    
+    state = {
+        pieces: []
+      }
+    
+      componentDidMount() {
+        this.loadiUnfnishedPieces();
+      }
+    
+      loadUnfinishedPieces() {
+        API.getUnfinishedPieces()
+          .then(res => this.setState({ pieces: res.data }));
+      }
+
 render() {
     return (
       <>
@@ -13,60 +27,17 @@ render() {
               <NewBlockPreview newblocklink={"/write/new"} />
             </Col>
             <Col size="md-4">
-              <BlockPreview
-                newBlockLink={"/write/1"}
-                title={"Last Night I Went"}
-                excerpt={
-                  "It never occured to Rachel that staying up all night would lead to her demise. She was so sick to his stomach the next day."
-                }
-                wordCount={25}
-                authorCount={1}
-              />
+            {this.state.pieces.map(piece => (
+          <WriteCard
+            id={piece.id}
+            key={piece.id}
+            title={piece.title}
+            excerpt={piece.excerpt}
+            authorCount={piece.authorCount}
+          />
+        ))}
             </Col>
-            <Col size="md-4">
-              <BlockPreview
-                newBlockLink={"/write/2"}
-                title={"If Ever in Trouble"}
-                excerpt={
-                  "Steve just did it! He never thought he would actually do such a thing. Nonetheless, he had completed the worse thing he had ever done."
-                }
-                wordCount={31}
-                authorCount={1}
-              />
-            </Col>
-            <Col size="md-4">
-              <BlockPreview
-                newBlockLink={"/write/3"}
-                title={"Strawberry and Mail"}
-                excerpt={
-                  "Two things you never thought would be in the same sentence. Was it weird? No one else seemed to think so. What was going on?"
-                }
-                wordCount={25}
-                authorCount={2}
-              />
-            </Col>
-            <Col size="md-4">
-              <BlockPreview
-                newBlockLink={"/write/4"}
-                title={"Almost Done"}
-                excerpt={
-                  "There were only two weeks of class remaining. It had to be done now. There could not be any hesitation. Any mistake would have beenn brutal."
-                }
-                wordCount={75}
-                authorCount={3}
-              />
-            </Col>
-            <Col size="md-4">
-              <BlockPreview
-                newBlockLink={"/write/5"}
-                title={"They Met at a Traffic Light"}
-                excerpt={
-                  "She just stared at him for a full minute. The light turned green, but she did not notice. It was as though time had froze."
-                }
-                wordCount={60}
-                authorCount={4}
-              />
-            </Col>
+            
           </Row>
         </Container>
       </>
